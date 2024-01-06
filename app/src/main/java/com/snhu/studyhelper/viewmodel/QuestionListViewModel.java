dd35fba5d3e949a2c39ee0a0b0abc12a44decc8c
Makayla Anderson-Tucker <makayla.anderson.tucker@gmail.com>
2024-01-06 13:23:59 -0800

    Subject and Question screens are developed and display stored questions. Next is to connect CRUD functionality for the subjects and questions.

--- app/src/main/java/com/snhu/studyhelper/viewmodel/QuestionListViewModel.java
+++ app/src/main/java/com/snhu/studyhelper/viewmodel/QuestionListViewModel.java
@@ -1,4 +1,19 @@
 package com.snhu.studyhelper.viewmodel;
 
+import android.app.Application;
+import com.snhu.studyhelper.model.Question;
+import com.snhu.studyhelper.repo.StudyRepository;
+import java.util.List;
+
 public class QuestionListViewModel {
+
+    private StudyRepository studyRepo;
+
+    public QuestionListViewModel(Application application) {
+        studyRepo = StudyRepository.getInstance(application.getApplicationContext());
+    }
+
+    public List<Question> getQuestions(long subjectId) {
+        return studyRepo.getQuestions(subjectId);
+    }
 }
